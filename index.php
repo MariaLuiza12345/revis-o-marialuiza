bc69cb4d11d140c55ba9996a90ffaa5e9e284f44
MARIA LUIZA LIMA DE OLIVEIRA <oliveira.0936@aluno.pr.senac.br>
2023-03-08 12:06:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>۫ ⋆ ࣪. ୧ Script em PHP ୨ ִ ۫ ⁎ . </title>
+    </head>
+    <body>
+
+    <?php
+
+        $pessoa1 = array();
+        $pessoa2 = array();
+        $pessoa3 = array();
+        $pessoa4 = array();
+
+        $pessoa1 [0][0][0][0][0][0] = "Joao";
+        $pessoa1 [0][0][0][0][1][1] = "16";
+        $pessoa1 [0][0][0][0][2][2] = "Sonserina";
+
+        $pessoa2 [0][0][0][0][0][0] = "Juan";
+        $pessoa2 [0][0][0][0][1][1] = "16";
+        $pessoa2 [0][0][0][0][2][2] = "Corvinal";
+
+        $pessoa3 [0][0][0][0][0][0] = "Malu";
+        $pessoa3 [0][0][0][0][1][1] = "17";
+        $pessoa3 [0][0][0][0][2][2] = "Sonserina";
+
+        $pessoa4 [0][0][0][0][0][0] = "Joaquim";
+        $pessoa4 [0][0][0][0][1][1] = "15";
+        $pessoa4 [0][0][0][0][2][2] = "Grifinória";
+
+        echo " O " . $pessoa1[0][0][0][0][0][0] . " tem " . $pessoa1[0][0][0][0][1][1] . " e é da casa da " . $pessoa1[0][0][0][0][2][2];
+        echo("</br>");
+        echo " O " . $pessoa2[0][0][0][0][0][0] . " tem " . $pessoa2[0][0][0][0][1][1] . " e é da casa da " . $pessoa2[0][0][0][0][2][2];
+        echo("</br>");
+        echo " O " . $pessoa3[0][0][0][0][0][0] . " tem " . $pessoa3[0][0][0][0][1][1] . " e é da casa da " . $pessoa3[0][0][0][0][2][2];
+        echo("</br>");
+        echo " O " . $pessoa4[0][0][0][0][0][0] . " tem " . $pessoa4[0][0][0][0][1][1] . " e é da casa da " . $pessoa4[0][0][0][0][2][2];
+
+    ?>
+    <body>
+</html>
